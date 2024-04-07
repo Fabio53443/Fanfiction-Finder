3eb7ebb4e9ecf25bafe6a5380654736fb42f8e8f
Fabio53443 <64356481+Fabio53443@users.noreply.github.com>
2024-04-07 11:03:48 +0200

    fixes

--- utils/metadata.py
+++ utils/metadata.py
@@ -14,9 +14,10 @@ from utils.search import get_ao3_url, get_fic_url
 URL_VALIDATE = r"(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?"
 
 def format_summary(sum: str):
+    rsum = sum.split(". ")
     res_string = ""
     for i in range(0, 3):
-        res_string += sum[i] + ". "
+        res_string += rsum[i] + ". "
     if len(res_string) < len(sum):
         res_string += "..."
     return res_string
